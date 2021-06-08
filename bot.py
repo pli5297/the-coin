e632a6389ce6c2c638d8f0a21f39fe0803e8f8fd
pli5297 <paulli5297@gmail.com>
2021-06-07 23:06:46 -0500

    Added rich embed message to team generator function

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ from typing import final
 import discord
 from discord import voice_client
 from discord import user
+from discord import team
 from discord.channel import VoiceChannel
 from dotenv import load_dotenv
 from discord.ext import commands
@@ -28,7 +29,8 @@ async def on_ready():
 async def coinflip(ctx):
     coin_list = ['hEaDs!', 'tAiLs!']
     response = random.choice(coin_list)
-    await ctx.send(response)
+    await ctx.channel.send()
+    await ctx.channel.send(response)
 
 @bot.command(name = 'ryan', help = 'rYaN')
 async def ryan(ctx):
@@ -50,32 +52,85 @@ async def rishi(ctx):
     imgdirectory = 'pictures\\'
     await ctx.channel.send(file=discord.File(imgdirectory + 'kameleon.jpg'))
 
-@bot.command(name = 'teams', help='')
+@bot.command(name = 'teams', help='Generates random teams for a custom game')
 async def teams(ctx):
     def createTeams(users):
-        """Returns a string describing the teams for the bot to send"""
-        ### NOTE: Will replace this with a rich embed message instead of just a string ###
-
+        """Returns a list of randomized players or an empty list if there are no players"""
         if not users:
             # If a team is attempted to be created with an empty list of users
-            return "Voice channel is empty"
+            return []
         
         random.shuffle(users) # Randomizes users passed in
         teamList = users[0:(len(users) // 2)] + users[(len(users) // 2):] # Creates the team given a list of users
 
-        finalTeams = "Team 1:"
-        for name in teamList[0:(len(teamList) // 2)]:
-            finalTeams += " " + name
+        # finalTeams = "Team 1:"
+        # for name in teamList[0:(len(teamList) // 2)]:
+        #     finalTeams += " " + name
         
-        finalTeams += "\nTeam 2:"
-        for name in teamList[(len(teamList) // 2):]:
-            finalTeams += " " + name
+        # finalTeams += "\nTeam 2:"
+        # for name in teamList[(len(teamList) // 2):]:
+        #     finalTeams += " " + name
 
-        return finalTeams
+        return teamList
+    
+    def chooseCharacters(game, teamLen):
+        '''Returns two strings containing each teams characters'''
+        # Randomizes players' agents
+        agentList = ["Reyna", "Omen", "Killjoy", "Brimstone", "Astra", "Sova", "Phoenix", "Jett", "Cypher", "Sage", "Raze", "Yoru", "Viper", "Skye", "Breach"]
+        teamOneAgents = random.sample(agentList, teamLen)
+        teamTwoAgents = random.sample(agentList, teamLen)
+        teamOneAgents = [(agent + "\n") for agent in teamOneAgents]
+        teamTwoAgents = [(agent + "\n") for agent in teamTwoAgents]
+        teamOneAgents = "".join(teamOneAgents)
+        teamTwoAgents = "".join(teamTwoAgents)
 
+        return teamOneAgents, teamTwoAgents
+
+    def createEmbed(game, teams):
+        '''Returns a rich embed message for the custom teams'''
+        if game == "Valorant":
+            # Gets the map for the game
+            mapsFile = "pictures\\valorant-maps.txt"
+            file = open(mapsFile, "r")
+            mapList = file.readlines()
+            mapList = [map.split(",") for map in mapList]
+            map = random.choice(mapList)
+            file.close()
+
+            # Divides out the randomized list of users into two strings containing each team
+            if len(teams) <= 1:
+                # Bounds checking
+                return None
+            teamOne = teams[0:(len(teams) // 2)]
+            teamTwo = teams[(len(teams) // 2):]
+            teamOne = [(user + "\n") for user in teamOne]
+            teamTwo = [(user + "\n") for user in teamTwo]
+            teamOneAgents, teamTwoAgents = chooseCharacters(game, len(teamOne))
+            teamOne = "".join(teamOne)
+            teamTwo = "".join(teamTwo)
+
+            # Creates the embed
+            embed = discord.Embed(title = map[0], type = "rich", color=0xff0000)
+            # Map Image
+            embed.set_thumbnail(url=map[1])
+            # Attackers Column
+            embed.add_field(name = "Attackers", value = teamOne, inline = True)
+            # Attackers Agents
+            embed.add_field(name = "Agents", value = teamOneAgents, inline = True)
+            # Empty Line Seperator
+            embed.add_field(name = "\u200B", value = "\u200B", inline = False)
+            # Defenders Column
+            embed.add_field(name = "Defenders", value = teamTwo, inline = True)
+            # Defenders Agents
+            embed.add_field(name = "Agents", value = teamTwoAgents, inline = True)
+
+        return embed
+
+    game = "Valorant"
     inVoiceChannel = ctx.author.voice # Voice status of user who called the command
     memberList = [] # List of members
     userList = [] # List of strings of the member's nicknames in the voice channel
+    
     
     if inVoiceChannel == None:
         # If member who called command is not in a voice channel
@@ -83,8 +138,15 @@ async def teams(ctx):
     else:
         voiceChannel = ctx.author.voice.channel # Gets the voice channel the author is in
         memberList = voiceChannel.members # Gets members in voice channel of the author
+        memberList = [member for member in memberList if (not member.voice.self_mute)]
+
         # Pulls strings of usernames/nicknames (if applicable)
         userList = [member.nick if (member.nick is not None) else member.name for member in memberList]
-        await ctx.channel.send(createTeams(userList))
+        teamList = createTeams(userList)
+        embed = createEmbed(game, teamList)
+        if embed != None:
+            await ctx.channel.send(embed = embed)
+        else:
+            await ctx.channel.send("Not enough players dumbass")
 
 bot.run(TOKEN)
